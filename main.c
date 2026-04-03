40795e3c7ad202fa867f4271d93d766df550be1a
Tran Phu An <AnPhuTran06@gmail.com>
2026-04-03 12:59:40 +0700

    add full energy manager system

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+typedef struct{
+    char name[50];
+    int power;        // cong suat(W)
+    int priority;     // 1 (cao) -> 5 (thap)
+    int state;        // 0: OFF, 1: ON
+    int mode;         // 0: nomal, 1: eco
+}Device;
+typedef struct{
+    float battery;    // %
+    int charging;     // 1: dang sac, 0: dang xa
+    float total_power;// tong cong suat
+}System;
+
+
 int main() {
     printf("Hello GitHub!\n");
     return 0;
